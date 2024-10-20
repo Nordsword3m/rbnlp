cf427021dbb891b22238f80bef3809140ff83331
Nordsword3m <doublethe3@gmail.com>
2024-10-20 23:35:07 +0100

    Change post to list

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from typing import List
 from fastapi import FastAPI
 from spacy import load
 from urllib.parse import unquote
 from pydantic import BaseModel
 class Item(BaseModel):
-    s: str
+  s: List[str]
 
 app = FastAPI()
 nlp = load("de_core_news_sm", disable=["parser", "morphologizer", "lemmatizer", "attribute_ruler", "ner"])
@@ -16,6 +17,6 @@ def read_root(s: str = None):
 @app.post("/")
 def read_root_post(item: Item = None):
   if item is not None and item.s is not None:
-    return nlp(item.s).to_json()
+    return [d.to_json() for d in nlp.pipe([unquote(x) for x in item.s])]
   
   return "use s key to analyze a text"
